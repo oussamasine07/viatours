e7098b104e551c878fe6dbfd3781cbb40bad51ec
oussamasine07 <sineoussama@gmail.com>
2025-01-02 12:56:07 +0100

    apply responsive for mobile screens

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const menuBtn = document.getElementById("menu-btn");
+const menuList = document.getElementById("menu-list");
+const menuClose = document.getElementById("close-list");
+
+
+menuBtn.addEventListener("click", () => {
+    menuList.style.display = "block";
+    
+    setTimeout(() => {
+        menuList.style.opacity = "1";
+        menuList.style.top = "70px";
+    }, 100);
+});
+
+menuClose.addEventListener("click", () => {
+    
+    menuList.style.top = "50px";
+    menuList.style.opacity = "0";
+
+    setTimeout(() => {
+        menuList.style.display = "none";
+    }, 100);
+});
