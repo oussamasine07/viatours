e2ea6c0840b0581277c4185fa84d982872349eaa
oussamasine07 <sineoussama@gmail.com>
2025-01-07 17:31:46 +0100

    delet log

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ menuClose.addEventListener("click", () => {
     menuList.style.top = "50px";
     menuList.style.opacity = "0";
 
-    console.log("testing merge")
+    
 
     setTimeout(() => {
         menuList.style.display = "none";
